7562746f4d54de687bfcddd18cc6cb6bdcac0564
VladSkopenko <skopirka2k17@gmail.com>
2023-10-07 18:42:28 +0300

    Add regularly expresscoins

---
+++ Module_one_1/From Go IT/7 ex.py
@@ -0,0 +1,7 @@
+# На цю мить у нас є три змінні: first_name, last_name та full_name
+
+# Додамо змінну message, яка фактично буде прототипом шаблонного листа користувачеві, який купив квиток. 
+#Цю змінну ми сформуємо за допомогою f-рядка. У змінну message ми, за допомогою f-рядка, помістимо рядок наступного змісту:
+
+# "Dear <підставляємо first_name>, we inform you that you have purchased a ticket to travel to the island of Mauritius.
+#  Departure June 31 of this year. Have a passport at <підставляємо full_name>. We are looking forward to seeing you!"
