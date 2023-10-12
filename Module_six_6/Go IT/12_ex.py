a503f04ed10a375c3df7b3330356be37a266c79a
VladSkopenko <skopirka2k17@gmail.com>
2023-10-12 07:54:22 +0300

    вариант №2

---
+++ Module_six_6/Go IT/12_ex.py
@@ -0,0 +1,22 @@
+# Функція get_credentials_users із попереднього завдання повертає нам список рядків username:password:
+
+# ['andry:uyro18890D', 'steve:oppjM13LL9e']
+# Реалізуйте функцію encode_data_to_base64(data), яка приймає у параметрі data 
+#зазначений список, виконує кодування кожної пари username:password у формат Base64 та повертає список із 
+#закодованими парами username:password у вигляді:
+
+# ['YW5kcnk6dXlybzE4ODkwRA==', 'c3RldmU6b3Bw
+
+import base64
+
+data = ['andry:uyro18890D', 'steve:oppjM13LL9e']
+def encode_data_to_base64(data):
+    my_str = ", ".join(data)
+    data_bytes = my_str.encode("utf-8")
+    base64_bytes = base64.b64encode(data_bytes)
+    base64_data = base64_bytes.decode('utf-8')
+    return  base64_data 
+
+
+print(encode_data_to_base64(data))
+    
