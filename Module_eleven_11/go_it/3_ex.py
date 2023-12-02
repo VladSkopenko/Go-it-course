41f3c3836d43a93bd36c97a5490073fa340ebb92
VladSkopenko <skopirka2k17@gmail.com>
2023-12-02 11:32:57 +0300

    11 mod 3 ex

--- Module_eleven_11/go_it/3_ex.py
+++ Module_eleven_11/go_it/3_ex.py
@@ -6,3 +6,33 @@
 #
 # print(point.x)  # None
 # print(point.y)  # 10
+
+class Point:
+    def __init__(self, x, y):
+        self.__x = None
+        self.__y = None
+        self.x = x
+        self.y = y
+
+    @property
+    def x(self):
+        return self.__x
+
+    @x.setter
+    def x(self, x):
+        if type(x) == int or type(x) == float:
+            self.__x = x
+        else:
+            self.__x = None
+
+    @property
+    def y(self):
+        return self.__y
+
+    @y.setter
+    def y(self, y):
+        if type(y) == int or type(y) == float:
+            self.__y = y
+        else:
+            self.__y = None
+
